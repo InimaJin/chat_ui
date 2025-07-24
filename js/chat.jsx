72936931a54c6ab20f4ec619b8e943a61742ded9
InimaJin <>
2025-07-24 18:05:10 +0200

    Automatic scrolling

--- js/chat.jsx
+++ js/chat.jsx
@@ -1,8 +1,10 @@
+import { useRef, useEffect, useState } from "react";
+
 import { updateStoredChat } from "./util";
 
 /* A single message in the chat history between the user and a contact. Graphical representation of an individual
  * message object in the chat array. */
-function Message({ userId, messageObj }) {
+function Message({ ref, userId, messageObj }) {
     const msgType = userId === messageObj.sender ? "msg-sent" : "msg-recv";
     const date = new Date(messageObj.timestamp);
     const timestamp = new Intl.DateTimeFormat(
@@ -15,7 +17,7 @@ function Message({ userId, messageObj }) {
         }
     ).format(date);
     return (
-        <div className={"message-wrapper " + msgType}>
+        <div ref={ref} className={"message-wrapper " + msgType}>
             <p className="msg-timestamp">{timestamp}</p>
             <div className="message-body">
                 {messageObj.text}
@@ -26,6 +28,21 @@ function Message({ userId, messageObj }) {
 
 /* Window for chat history and text input. */
 export function ChatWindow({ userData, activeContact, chat, setChat, contacts, setContacts }) {
+    const latestMessageRef = useRef(null);
+    function scrollToLatestMessage() {
+        latestMessageRef.current.scrollIntoView({behavior: "instant", block: "end"});
+    }
+
+    const [scrollDown, setScrollDown] = useState(false)
+    const chatInputRef = useRef(null);
+    useEffect(() => {
+        chatInputRef.current.focus();
+        if (latestMessageRef.current) {
+            scrollToLatestMessage();
+        }
+    }, [activeContact, scrollDown]);
+
+
     const contactData = contacts.find(c => c.id === activeContact);
 
     function onTextChange(newText) {
@@ -55,13 +72,17 @@ export function ChatWindow({ userData, activeContact, chat, setChat, contacts, s
             };
             const nextChat = [...chat, sentMessage, reply];
 
+            setScrollDown(!scrollDown);
             setChat(nextChat);
             updateStoredChat(userData.id, activeContact, sentMessage, reply);
             onTextChange("");
         }
     }
 
-    const messages = chat.map(o => <Message key={o.msgId} userId={userData.id} messageObj={o}/>);
+    const messages = chat.map((o, i) => {
+        const ref = i === chat.length - 1 ? latestMessageRef : null;
+        return <Message key={o.msgId} ref={ref} userId={userData.id} messageObj={o}/>;
+    });
 
     return (
         <>
@@ -69,10 +90,10 @@ export function ChatWindow({ userData, activeContact, chat, setChat, contacts, s
                 {messages}
             </div>
             <form className="chat-form">
-                <textarea className="message-input box"
+                <textarea ref={chatInputRef} className="message-input box"
                     value={contactData.currentMessage}
                     onChange={(e) => onTextChange(e.target.value)}
-                    autoFocus />
+                />
                 <div>
                     <button type="submit" onClick={(e) => {
                         e.preventDefault();
